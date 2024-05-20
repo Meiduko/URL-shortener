be9f0754265acb506b0d08a1f4ff9a99beafa8c8
Borja González <borjago.bgo@gmail.com>
2024-05-20 22:40:51 +0200

    First commit

---
+++ url-shortener/resources/js/Pages/Home.tsx
@@ -0,0 +1,11 @@
+import Guest from "@/Layouts/GuestLayout";
+
+export default function Welcome() {
+    return (
+        <>
+            <Guest>
+                <h1 className="underline">Hello world</h1>
+            </Guest>
+        </>
+    );
+}
---
+++ url-shortener/routes/web.php
@@ -0,0 +1,6 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::inertia('/', 'Home');
+Route::inertia('/about', 'Dashboard');
